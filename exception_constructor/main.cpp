14f86f9057b4f3f3bfe6f30f680033cbfe5a9319
simplydesigner <simplydesigner@ya.ru>
2016-08-22 21:06:38 +0300

    initialized

--- exception_constructor/main.cpp
+++ exception_constructor/main.cpp
@@ -1,15 +1,41 @@
-//
-//  main.cpp
-//  exception_constructor
-//
-//  Created by simplydesigner on 22/08/16.
-//  Copyright © 2016 JustDevelopers. All rights reserved.
-//
-
 #include <iostream>
 
+struct attribute_t
+{
+    attribute_t()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    ~attribute_t()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+};
+
+class base_t
+{
+public:
+    base_t()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        throw std::logic_error(__PRETTY_FUNCTION__);
+    }
+    ~base_t()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        throw std::logic_error(__PRETTY_FUNCTION__);
+    }
+private:
+    attribute_t attribute;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    try {
+        base_t base;
+    } catch (...) {
+    }
+    
+    
     return 0;
 }
